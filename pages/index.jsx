da5facc6fd386f73eb0f4b1bd1421c8b41f28817
wessan001 <wesleyortegassan@gmail.com>
2024-11-01 14:06:08 -0300

    Landing Page, Header Feita

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import Header from "@/components/header";
+import styles from "./Home.module.scss";
+
+export default function Home(){
+  return <div className="{styles.container}">
+  <Header />
+  </div>
+}
+
