0d78b3e7744dbbc16bd2adc36929c082482d531c
wessan001 <wesleyortegassan@gmail.com>
2024-11-04 10:31:36 -0300

    Welcome session

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import Link from "next/link"
 import Logo from "/public/images/agencia.svg"
 import Styles from "./header.module.scss"
+import Button from "../button"
 
 const Header = () => {
     return <div className={Styles.container}>
@@ -14,8 +15,9 @@ const Header = () => {
                 <Link href="#">Cases</Link>
 
     </div>
-    <div className={Styles.action}></div>
-    <button className={Styles.button}>Fale conosco</button>
+    <div className={Styles.action}>
+        <Button title="Fale conosco"/>
+    </div>
     </div>
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
 import styles from "./Home.module.scss";
+import Welcome from "@/components/welcome";
 
 export default function Home(){
   return <div className="{styles.container}">
   <Header />
+  <Welcome />
   </div>
 }
 
